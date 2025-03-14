673a8b4e20b30a13d32a3bed2a7becc48983ec22
CD1MUL <148664936+CD1MUL@users.noreply.github.com>
2025-03-13 23:26:14 -0300

    app.js

--- app.js
+++ app.js
@@ -1,57 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// para desarrollar la actividad se usoron fuentes de videos Alura, Trello, developer.mozilla, YouTube entre otros.
-// INTENTO 1
+//INTENTO 3
 
-//  Creando array
-//let amigo = [];
+let amigos = []; 
+console.log (amigos.length);
 
-//Realizando función con getElement
-//function añadirAmigo()  {
-  //  let nombreHumano = document.getElementById("amigo").value;
-   // if (nombreHumano.trim() === ""){
-     //   alert("Por favor, inserte un nombre.");
-    //} else {
-      //  amigo.push(añadirAmigo);
-        //document.querySelector("#amigo").value = "";
-        //verListaAmigo ();
-   // }
-//
-//}
-// Función lista de amigo en la pantalla
-
-////function verListaAmigo(){
-    //let listaAmigoSorteados = document.querySelector("#listaAmigoSorteados");
-    //listaAmigoSorteados.innerHTML="";
-    //for ( let i=0; i< listaAmigoSorteados.length; i++){
-      //  let nLista = document.createElement("li");
-        //nLista.innerHTML = listaSorteo[i];
-        //listaAmigoSorteados.appendChild(nLista);
-   // }
-//}
-
-// INTENTO 2 VAMOS QUE SE PUEDE !!!
-// 
-
-
-const inputAmigo = document.getElementById("amigo");
-const listaAmigos = [];
-const ulListaAmigo = document.getElementById("listaAmigos");
-const ulResultado = document.getElementById("resultado");
-
-// Función
-function agregarAmigo() { 
-  if(inputAmigo.value == ""){
-      alert("Por favor, inserte un nombre.")
-  }
-    listaAmigos.push(inputAmigo.value);
-    ulListaAmigo.innerHTML += `<li>${inputAmigo.value}</li>`;
+//Añadir la función
+function agregarAmigo (){
     
-};
-
-//Sortear amigo
-
-function sortearAmigo(){
-  const random = Math.floor(Math.random() * listaAmigos.length);
-  const amigoSecreto = listaAmigos[random];
-   ulResultado.innerHTML = `<li>El amigo secreto ${amigoSecreto}</li>`;
+    let nombreAmigo = document.getElementById ('amigo').value;
+    console.log(nombreAmigo);
+    console.log(typeof(nombreAmigo));
+   
+    if (nombreAmigo.trim() === "") {
+            alert("Por favor, inserte un nombre.");
+        } else {
+            amigos.push(nombreAmigo); 
+            actualizarLista(); 
+            console.log(amigos);
+            document.getElementById('amigo').value = ""; 
+        }
 }
+
+function actualizarLista(){ 
+    const listaElement = document.getElementById ('listaAmigos'); 
+    listaElement.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++){ 
+        const li = document.createElement('li');
+        li.textContent = amigos[i]; 
+        listaElement.appendChild(li); 
+        console.log(amigos);   
+    }
+}
+function sortearAmigo(){
+    const resultadoElement = document.getElementById('resultado');   
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length); 
+    const amigoSorteado = amigos [indiceAleatorio]; 
+    console.log ("Amigo Sorteado:" + amigoSorteado);
+    resultadoElement.innerHTML = "El amigo secreto es: " + amigoSorteado;
+    }
+
